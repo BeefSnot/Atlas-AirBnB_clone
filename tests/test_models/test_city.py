09d89bb437599b213c9c251c4c4a8f6cb0ededb6
BeefSnot <161774551+BeefSnot@users.noreply.github.com>
2024-10-02 14:09:25 -0500

    Update test_city.py

--- tests/test_models/test_city.py
+++ tests/test_models/test_city.py
@@ -1,39 +1,15 @@
 #!/usr/bin/python3
-""" Test city """
+import unittest
+from models.city import City
+import datetime
 
-import unittests
-import pep8
-import os
-from models import City
-from models.base_model import BaseModel
-
-class Test_City(unittest.TestCase):
-    """ Tests city """
-
-    def test_pep8_City(self):
-        """Tests pep8 style"""
-        style = pep8.StyleGuide(quiet=True)
-        p = style.check_files(['models/city.py'])
-        self.assertEqual(p.total_errors, 0, "Check pep8")
-
-    def test_City_dict(self):
-        """ City_dict """
-        self.assertTrue('id' in self.city.__dict__)
-        self.assertTrue('created_at' in self.city.__dict__)
-        self.assertTrue('updated_at' in self.city.__dict__)
-        self.assertTrue('state_id' in self.city.__dict__)
-        self.assertTrue('name' in self.city.__dict__)
-        self.assertTrue('__class__' in self.city.__dict__)
+"""Defines a class TestCity"""
 
 
-    def test_save_City(self):
-        """ save_city """
-        self.city.save()
-        self.assertNotEqual(self.city.created_at, self.city.updated_at)
-
-    def test_inst(self):
-        """ test_inst"""
-        self.assertIsInstance(self.new_city, City)
-
-if __name__ == "__main__":
-    unittest.main()
+class TestCity(unittest.TestCase):
+    """Class used for test cases"""
+    def test_city(self):
+        """Test case for City"""
+        self.city = City()
+        self.assertTrue(hasattr(self.city, 'state_id'))
+        self.assertEqual(self.city.name, "")

9c9ad446468b4ec664d92c7885248242ddf9078b
BeefSnot <161774551+BeefSnot@users.noreply.github.com>
2024-10-02 12:39:21 -0500

    Update test_amenity.py

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -1,33 +1,14 @@
-#!/usr/bin/python3
-""" Tests Amenity """
-
+#!usr/bin/python3
 import unittest
-import os
-import pep8
+from models.amenity import Amenity
 import datetime
-from models import amenity
-Amenity = amenity.Amenity
 
-class Test_Amenity(unittest.TestCase):
-    """ Tests amenity """
+"""Defines a class TestAmenity"""
 
-    def test_pep8(self):
-        """ Tests the pep8 """
-        pep8style = pep8.StyleGuide(quiet=True)
-        result = pep8style.check_files(['models/amenity.py'])
-        self.assertEqual(result.total_errors, 0, "Check pep8")
-
-    def test_Amenity_dict(self):
-        """Amenity_dict"""
-        self.assertTrue('id' in self.amenity.__dict__)
-        self.assertTrue('created_at' in self.amenity.__dict__)
-        self.assertTrue('updated_at' in self.amenity.__dict__)
-        self.assertTrue('name' in self.amenity.__dict__)
-
-    def test_save_Amenity(self):
-        """Amenity save"""
-        self.amenity.save()
-        self.assertNotEqual(self.amenity.created_at, self.amenity.updated_at)
-
-if __name__ == '__main__':
-    unittest.main()
+class TestAmenity(unittest.TestCase):
+    """Class used for test cases"""
+    def test_amenity(self):
+        """Test case for Amenity"""
+        self.amenity = Amenity()
+        self.assertTrue(hasattr(self.amenity, 'name'))
+        self.assertEqual(self.amenity.name, "")

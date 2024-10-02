b197b7950dca4c4a564dbe1cbdb3bc10110d7fd3
BeefSnot <161774551+BeefSnot@users.noreply.github.com>
2024-10-02 14:23:05 -0500

    Update file_storage.py

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,95 +1,89 @@
 #!/usr/bin/python3
-"""Module for FileStorage class."""
-import datetime
 import json
 import os
+from models.base_model import BaseModel
+from models.user import User
 
 
 class FileStorage:
+    """Class to serialize and deserialize instances to/from a JSON file."""
 
-    """Class for storing and retrieving data"""
-    __file_path = "file.json"
-    __objects = {}
+    def __init__(self):
+        self.__file_path = "file.json"  # path to the JSON file
+        self.__objects = {}  # stores all objects
+
+    # @property
+    # def objects(self):
+        # """Expose __objects as a property."""
+        # return self.__objects
 
     def all(self):
-        """returns the dictionary __objects"""
-        return FileStorage.__objects
+        """Returns the dictionary __objects."""
+        return self.__objects
 
     def new(self, obj):
-        """sets in __objects the obj with key <obj class name>.id"""
-        key = "{}.{}".format(type(obj).__name__, obj.id)
-        FileStorage.__objects[key] = obj
+        """Sets in __objects the obj with key <obj class name>.id."""
+        key = f"{obj.__class__.__name__}.{obj.id}"  # Fixed key formatting
+        self.__objects[key] = obj  # Ensure the object is properly stored
 
     def save(self):
-        """ serializes __objects to the JSON file (path: __file_path)"""
-        with open(FileStorage.__file_path, "w", encoding="utf-8") as f:
-            d = {k: v.to_dict() for k, v in FileStorage.__objects.items()}
-            json.dump(d, f)
-
-    def classes(self):
-        """Returns a dictionary of valid classes and their references"""
-        from models.base_model import BaseModel
-        from models.user import User
-        from models.state import State
-        from models.city import City
-        from models.amenity import Amenity
-        from models.place import Place
-        from models.review import Review
-
-        classes = {"BaseModel": BaseModel,
-                   "User": User,
-                   "State": State,
-                   "City": City,
-                   "Amenity": Amenity,
-                   "Place": Place,
-                   "Review": Review}
-        return classes
+        """Serializes __objects to the JSON file."""
+        try:
+            with open(self.__file_path, 'w') as f:
+                # Create a dictionary representation of all objects
+                data = {key: obj.to_dict() for key,
+                        obj in self.__objects.items()}
+                # Serialize the data to JSON and write it to the file
+                json.dump(data, f)
+            return True  # Ensures that it returns True after saving
+        except (IOError, json.JSONDecodeError):
+            return False  # Return False if there was an error
+        except Exception as e:
+            return False  # Handle any other exception
 
     def reload(self):
-        """Reloads the stored objects"""
-        if not os.path.isfile(FileStorage.__file_path):
-            return
-        with open(FileStorage.__file_path, "r", encoding="utf-8") as f:
-            obj_dict = json.load(f)
-            obj_dict = {k: self.classes()[v["__class__"]](**v)
-                        for k, v in obj_dict.items()}
-            # TODO: should this overwrite or insert?
-            FileStorage.__objects = obj_dict
+        """Deserializes the JSON file to __objects."""
+        if os.path.exists(self.__file_path):
+            with open(self.__file_path, 'r') as f:
+                loaded_data = json.load(f)  # Load JSON data
+                for key, value in loaded_data.items():
+                    class_name = value.pop("__class__", None)
+                    # Remove __class__ key
 
-    def attributes(self):
-        """Returns the valid attributes and their types for classname"""
-        attributes = {
-            "BaseModel":
-                     {"id": str,
-                      "created_at": datetime.datetime,
-                      "updated_at": datetime.datetime},
-            "User":
-                     {"email": str,
-                      "password": str,
-                      "first_name": str,
-                      "last_name": str},
-            "State":
-                     {"name": str},
-            "City":
-                     {"state_id": str,
-                      "name": str},
-            "Amenity":
-                     {"name": str},
-            "Place":
-                     {"city_id": str,
-                      "user_id": str,
-                      "name": str,
-                      "description": str,
-                      "number_rooms": int,
-                      "number_bathrooms": int,
-                      "max_guest": int,
-                      "price_by_night": int,
-                      "latitude": float,
-                      "longitude": float,
-                      "amenity_ids": list},
-            "Review":
-            {"place_id": str,
-                         "user_id": str,
-                         "text": str}
-        }
-        return attributes
+                    if not class_name:
+                        continue
+
+                    # Import classes here to avoid circular import
+                    from models.base_model import BaseModel
+                    from models.user import User
+                    from models.place import Place
+                    from models.state import State
+                    from models.city import City
+                    from models.review import Review
+                    from models.amenity import Amenity
+
+                # Map class names to actual classes
+                classes = {
+                    "BaseModel": BaseModel,
+                    "User": User,
+                    "Place": Place,
+                    "State": State,
+                    "City": City,
+                    "Review": Review,
+                    "Amenity": Amenity
+                }
+
+                # Ensure class name is retrieved correctly from the dictionary
+                if class_name in classes:
+                    cls = classes.get(class_name)
+                    # Retrieve the class object from the classes dictionary
+                    self.objects[key] = cls(**value)
+                    # Instantiate the class using the saved dictionary
+
+    def get_objects(self):
+        """Returns the objects stored in __objects."""
+        return self.__objects  # Accessing __objects directly is fine here
+
+    def count_objects(self):
+        """Returns the number of objects stored."""
+        return len(self.__objects)
